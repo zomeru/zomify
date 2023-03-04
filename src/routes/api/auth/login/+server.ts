3a3bf1a33da78777c707b40b53d7f55c11587760
zomeru <zomerzxc.19@gmail.com>
2023-03-05 04:46:22 +0800

    Add authorization process

---
+++ src/routes/api/auth/login/+server.ts
@@ -0,0 +1,38 @@
+import { redirect } from '@sveltejs/kit';
+import pkce from 'pkce-gen';
+import { SPOTIFY_APP_CLIENT_ID, BASE_URL } from '$env/static/private';
+import type { RequestHandler } from './$types';
+
+const generateRandomString = (length: number) => {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (let i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+};
+
+const state = generateRandomString(16);
+const challenge = pkce.create();
+
+const scope =
+	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
+
+export const GET: RequestHandler = ({ cookies }) => {
+	cookies.set('spotify_auth_state', state);
+	cookies.set('spotify_auth_challenge_verifier', challenge.code_verifier);
+
+	throw redirect(
+		307,
+		`https://accounts.spotify.com/authorize?${new URLSearchParams({
+			response_type: 'code',
+			client_id: SPOTIFY_APP_CLIENT_ID,
+			scope,
+			redirect_uri: `${BASE_URL}/api/auth/callback`,
+			state,
+			code_challenge_method: 'S256',
+			code_challenge: challenge.code_challenge
+		})}`
+	);
+};
