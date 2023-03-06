390f8251b99e7db6637dc69014d2cd646406b287
zomeru <zomerzxc.19@gmail.com>
2023-03-07 05:18:12 +0800

    fetch all the needed data in page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+
+  $: console.log(data);
+</script>
+
 
 <div class="hello-world min-h-[2000px]">Hello world!</div>
 
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,47 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/spotify/browse/new-releases?limit=6');
-	const response2 = await fetch('/api/spotify/browse/featured-playlists');
+export const load: PageLoad = async ({ fetch, parent }) => {
+	const { user } = await parent();
 
-	console.log({
-		1: await response.json(),
-		2: await response2.json()
-	});
+	const dataUrls = {
+		newReleases: '/browse/new-releases?limit=6',
+		featuredPlaylists: '/browse/featured-playlists?limit=6',
+		userPlaylists: `/users/${user?.id}/playlists?limit=6`
+	};
+
+	const categoriesResponse = await fetch('/api/spotify/browse/categories');
+	const categoriesResponseJSON: SpotifyApi.MultipleCategoriesResponse | undefined =
+		categoriesResponse.ok ? await categoriesResponse.json() : undefined;
+
+	const randomCategories = categoriesResponseJSON
+		? [...categoriesResponseJSON.categories.items].sort(() => 0.5 - Math.random()).slice(0, 3)
+		: [];
+
+	const randomCategoriesPromises = randomCategories.map((category) =>
+		fetch(`/api/spotify/browse/categories/${category.id}/playlists?limit=6`)
+	);
+
+	const [newReleases, featuredPlaylists, userPlaylists, ...randomCategoriesResponse] =
+		await Promise.all([
+			...Object.values(dataUrls).map((url) => fetch(`/api/spotify${url}`)),
+			...randomCategoriesPromises
+		]);
+
+	return {
+		newReleases: newReleases.ok
+			? (newReleases.json() as Promise<SpotifyApi.ListOfNewReleasesResponse>)
+			: undefined,
+		featuredPlaylists: featuredPlaylists.ok
+			? (featuredPlaylists.json() as Promise<SpotifyApi.ListOfFeaturedPlaylistsResponse>)
+			: undefined,
+		userPlaylists: userPlaylists.ok
+			? (userPlaylists.json() as Promise<SpotifyApi.ListOfUsersPlaylistsResponse>)
+			: undefined,
+		homeCategories: randomCategories,
+		categoriesPlaylists: Promise.all(
+			randomCategoriesResponse.map((response) =>
+				response.ok ? (response.json() as Promise<SpotifyApi.CategoryPlaylistsResponse>) : undefined
+			)
+		)
+	};
 };
