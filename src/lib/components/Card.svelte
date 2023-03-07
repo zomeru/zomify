c264d93113beea1cdbd9200ea9deebf87e661db2
zomeru <zomerzxc.19@gmail.com>
2023-03-08 02:53:16 +0800

    Add card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -8,7 +8,9 @@
 	export let item: AlbumType | PlaylistType | ArtistType;
 </script>
 
-<div class="card bg-darkGray p-3 rounded-md default-transition {item.type} hover:bg-mediumGray">
+<div
+	class="card bg-darkGray p-3 rounded-md default-transition {item.type} hover:bg-mediumGray h-full relative"
+>
 	{#if item.images.length > 0}
 		<!-- {#if 0 > 0} -->
 		<img
@@ -23,6 +25,23 @@
 		>
 			<Music aria-hidden="true" focusable="false" class="w-[40%] h-[40%] text-lightGray" />
 		</div>
+	{/if}
+	<h4 class="mb-3 text-base font-semibold leading-none truncate-1 ">
+		<a
+			class="text-textColor after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:rounded-md focus-visible:outline-none focus-visbile:after:outline-2 focus-visible:after:outline-accentColor focus-visible:after:outline"
+			href="/{item.type}/{item.id}"
+			title={item.name}
+		>
+			{item.name}
+		</a>
+	</h4>
+	{#if item.type === 'album'}
+		<p class="m-0 text-lightGray text-sm relative truncate-1">
+			{item.artists.map((item) => item.name).join(', ')}
+		</p>
+	{/if}
+	{#if item.type === 'playlist'}
+		<p class="m-0 text-lightGray text-sm relative truncate-2">{@html item.description}</p>
 	{/if}
 </div>
 
