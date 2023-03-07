8cad7572e5ba044f8ea3d88273e219b59f5b70af
zomeru <zomerzxc.19@gmail.com>
2023-03-08 02:30:16 +0800

    Add markup in home page

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Music } from 'lucide-svelte';
+
+	type AlbumType = SpotifyApi.AlbumObjectFull | SpotifyApi.AlbumObjectSimplified;
+	type PlaylistType = SpotifyApi.PlaylistObjectFull | SpotifyApi.PlaylistObjectSimplified;
+	type ArtistType = SpotifyApi.ArtistObjectFull;
+
+	export let item: AlbumType | PlaylistType | ArtistType;
+</script>
+
+<div class="card bg-darkGray p-3 rounded-md default-transition {item.type} hover:bg-mediumGray">
+	{#if item.images.length > 0}
+		<!-- {#if 0 > 0} -->
+		<img
+			loading="lazy"
+			src={item.images[0].url}
+			alt="{item.type} cover for {item.name}"
+			class="w-full aspect-square object-cover mb-5"
+		/>
+	{:else}
+		<div
+			class="cover-placeholder w-full aspect-square flex items-center justify-center bg-mediumGray mb-5 default-transition hover:bg-darkGray"
+		>
+			<Music aria-hidden="true" focusable="false" class="w-[40%] h-[40%] text-lightGray" />
+		</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	.card {
+		box-shadow: 0 0 40px rgba(0, 0, 0, 0.4);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,90 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+	import { Button, Card } from '$components';
+	import type { PageData } from './$types';
 
-  export let data: PageData;
+	export let data: PageData;
 
-  $: console.log(data);
+	$: console.log(data);
+
+	let sections: {
+		title: string;
+		path: string;
+		items: (SpotifyApi.AlbumObjectSimplified | SpotifyApi.PlaylistObjectSimplified)[];
+	}[] = [];
+
+	$: {
+		if (data.newReleases) {
+			sections = [
+				...sections,
+				{
+					title: 'New Releases',
+					path: '/sections/new-releases',
+					items: data.newReleases.albums.items
+				}
+			];
+		}
+
+		if (data.featuredPlaylists) {
+			sections = [
+				...sections,
+				{
+					title: 'Featured Playlists',
+					path: '/sections/featured-playlists',
+					items: data.featuredPlaylists.playlists.items
+				}
+			];
+		}
+
+		data.homeCategories.forEach((category, index) => {
+			const categoryPlaylist = data.categoriesPlaylists[index];
+			if (categoryPlaylist) {
+				sections = [
+					...sections,
+					{
+						title: category.name,
+						path: `/category/${category.id}`,
+						items: categoryPlaylist.playlists.items
+					}
+				];
+			}
+		});
+		if (data.userPlaylists) {
+			sections = [
+				...sections,
+				{
+					title: 'Your Playlists',
+					path: '/playlists',
+					items: data.userPlaylists.items
+				}
+			];
+		}
+	}
+
+	$: console.log(sections);
 </script>
 
-
-<div class="hello-world min-h-[2000px]">Hello world!</div>
+{#each sections as section}
+	<section class="content-row mb-10">
+		<div class="content-row-header flex items-center justify-between mb-5">
+			<div class="left">
+				<h2 class="section-title text-2xl m-0">{section.title}</h2>
+			</div>
+			<div class="right">
+				<Button element="a" href={section.path} variant="outline"
+					>See All
+					<span class="visually-hidden">{section.title}</span>
+				</Button>
+			</div>
+		</div>
+		<div class="grid-items">
+			{#each section.items as item}
+				<div class="grid-item">
+					<Card {item} />
+				</div>
+			{/each}
+		</div>
+	</section>
+{/each}
 
 <style lang="scss">
-	.hello-world {
-		color: red;
-	}
 </style>
